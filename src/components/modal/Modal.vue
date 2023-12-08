02cbe927406e5db4a0f5d906b413d3fa7bc18f2a
BaeCheolHan <joker10421@gmail.com>
2023-12-08 12:27:19 +0900

    UI Update

--- src/components/modal/Modal.vue
+++ src/components/modal/Modal.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="modal">
     <div class="overlay" @click="$emit('close-modal')"></div>
-    <div class="modal-card">
+    <div class="modal-card" :style="this.UiService.isMobile() ? '' : 'max-width: 1300px'">
       <slot />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
