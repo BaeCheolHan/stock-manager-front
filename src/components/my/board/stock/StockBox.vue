01a5d5a40c6b1b8fd4a918dde3618983b949c4ed
BaeCheolHan <joker10421@gmail.com>
2025-08-29 19:22:24 +0900

    :bug: ui 버그 수정

--- src/components/my/board/stock/StockBox.vue
+++ src/components/my/board/stock/StockBox.vue
@@ -18,8 +18,7 @@
                         white-space: nowrap;
                         max-width: 40%;
                         word-break: break-all;">{{ stock.name }}</p>
-              <p>({{ stock.symbol }} <span :class="UiService().setUpDownArrowClass(stock.compareToYesterdaySign)"
-                                                            :style="UiService().setColorStyle(stock.compareToYesterdaySign)">
+              <p>({{ stock.symbol }} <span :style="UiService().setColorStyle(stock.compareToYesterdaySign)">
                 {{ Math.floor(((stock.compareToYesterday / stock.nowPrice) * 100) * 100) / 100 }}%</span>)</p>
             </div>
             <div class="flex" style="justify-content: space-between; font-size: 12px;">
